8028cc7400db6fda95fa8267fd97a22a832f94cb
raouf <khourianes00@gmail.com>
2026-03-07 08:52:47 +0100

    s

--- train-segmentation.py
+++ train-segmentation.py
@@ -19,3 +19,27 @@ def train():
 
 if __name__ == "__main__":
     train()
+
+# from ultralytics import YOLO
+#
+# def train():
+#     model = YOLO("yolov8m-seg.pt")
+#
+#     model.train(
+#         data="dataset.yaml",
+#         epochs=100,
+#         imgsz=256,
+#         batch=24,            # auto batch size
+#         optimizer="AdamW",
+#         lr0=5e-4,
+#         weight_decay=5e-4,
+#         patience=20,
+#         device="0",       # auto GPU / CPU
+#         project="runs_segmentation",
+#         name="plant_disease_seg",
+#         exist_ok=True,
+#         workers=8            # speed up loading
+#     )
+#
+# if __name__ == "__main__":
+#     train()
